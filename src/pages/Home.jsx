df183c2feeff5cfd2e652184da47b8fec6be3276
gela samsonidze <samsonidze7@yahoo.com>
2023-04-25 12:12:49 +0400

    links update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,20 +12,25 @@ const Home = () => {
 
         <div className="home-links-container">
           <div className="home-links">
+            <small>Sign in pages</small>
             <Link to="/login">Login</Link>
             <Link to="/register">Register</Link>
             <Link to="/prices">Prices</Link>
+
+            <small className="mt-3">Non table pages</small>
             <Link to="/main-dashboard">Main Dashboard</Link>
             <Link to="/invoices1">Invoces v1</Link>
             <Link to="/invoices2">Invoces v2</Link>
             <Link to="/profile">profile</Link>
             <Link to="/employees">Employees</Link>
             <Link to="/vendors">vendors</Link>
-            <small className="mt-4">Old Tables</small>
+
+            <small className="mt-3">Old Tables</small>
             <Link to="/dashboard">dashboard table old</Link>
             <Link to="/all-orders">all orders table </Link>
           </div>
           <div className="home-links">
+            <small>New Tables</small>
             <Link to="/ag-table">ag table</Link>
             <Link to="/order-details">Order Details</Link>
             <Link to="/invoices-table">Invoices Table</Link>
