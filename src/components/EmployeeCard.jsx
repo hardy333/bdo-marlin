fa99ad3c30da50239a8b3a827597013c2d2124bc
gela samsonidze <samsonidze7@yahoo.com>
2023-04-25 16:47:04 +0400

    employee card menu

--- src/components/EmployeeCard.jsx
+++ src/components/EmployeeCard.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import user from "../assets/employees/user.svg";
 import dots from "../assets/employees/dots.svg";
 import userCircle from "../assets/marlin-icons/purple-user.svg";
+import { Menu, MenuItem } from "@szhsin/react-menu";
 
 console.log(userCircle);
 
@@ -11,7 +12,18 @@ const EmployeeCard = () => {
       <div className="employee-card__img-container">
         <img src={userCircle} className="employee-card__img" alt="" />
       </div>
-      <img className="employee-card__dots" src={dots} alt="" />
+
+      <Menu
+        direction="bottom"
+        align="center"
+        className="employee-card-menu"
+        menuButton={<img className="employee-card__dots" src={dots} alt="" />}
+        transition
+      >
+        <MenuItem>1</MenuItem>
+        <MenuItem>2</MenuItem>
+        <MenuItem>3</MenuItem>
+      </Menu>
 
       <h2>Mariam Mamulia</h2>
       <h3>Aadministrator</h3>
