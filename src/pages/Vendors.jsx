b5cd33f726a1036243b8eea16c33fad120937e2c
gela samsonidze <samsonidze14@gmail.com>
2023-04-28 11:09:19 +0400

    vendors refactor

--- src/pages/Vendors.jsx
+++ src/pages/Vendors.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import DashboardLayout from "../layout/DashboardLayout";
 import VendorsCard from "../components/VendorsCard";
 
@@ -11,6 +11,12 @@ import "../styles/vendors.css";
 import SearchSvg from "../components/svgs/SearchSvg";
 
 const Vendors = () => {
+  const [vendorArr, setVendorArr] = useState([
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  ]);
+
+  const [isChecked, setISChecked] = useState(true);
+
   return (
     <DashboardLayout>
       <section className="vendors">
@@ -19,21 +25,17 @@ const Vendors = () => {
           <div className="vendors-switch-container">
             <p>My Vendors</p>
             <div className="toggle-switch">
-              <input className="toggle-input" id="toggle" type="checkbox" />
+              <input
+                className="toggle-input"
+                checked={isChecked}
+                onChange={() => setISChecked(!isChecked)}
+                id="toggle"
+                type="checkbox"
+              />
               <label className="toggle-label" htmlFor="toggle"></label>
             </div>
             <p>All Vendors</p>
           </div>
-          {/* 2 */}
-          {/* <div className="vendors-btns-container">
-            <button>
-              <img src={filterSvg} alt="" />
-            </button>
-            <button>
-              <img src={listSortSvg} alt="" />
-            </button>
-          </div> */}
-          {/* 3 */}
           <div>
             <div className="input-wrapper">
               <input type="text" className="input" />
@@ -43,28 +45,15 @@ const Vendors = () => {
         </header>
 
         <div className="vendors-card-container">
-          <VendorsCard variant="active" />
-          <VendorsCard variant="active" />
-          <VendorsCard variant="active" />
-          <VendorsCard variant="active" />
-          <VendorsCard variant="active" />
-          <VendorsCard variant="active" />
-          <VendorsCard />
-          <VendorsCard />
-          <VendorsCard />
-          <VendorsCard />
-          <VendorsCard />
-          <VendorsCard />
-          <VendorsCard />
-          <VendorsCard />
-          <VendorsCard />
-          <VendorsCard />
-          <VendorsCard />
-          <VendorsCard />
-          <VendorsCard />
-          <VendorsCard />
-          <VendorsCard />
-          <VendorsCard />
+          {vendorArr
+            .filter((num) => (isChecked ? true : num === 1))
+            .map((num, index) => {
+              return num === 1 ? (
+                <VendorsCard key={index} variant="active" />
+              ) : (
+                <VendorsCard key={index} />
+              );
+            })}
         </div>
         <div className="employee-pag-container">
           <button>&larr;</button>
