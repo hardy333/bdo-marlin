988f2e190a5a84c5fd24cf1abfd758b2b53b8db0
gela samsonidze <samsonidze14@gmail.com>
2023-05-29 11:14:18 +0400

    temrs, contract

--- src/pages/Contract.jsx
+++ src/pages/Contract.jsx
@@ -4,13 +4,15 @@ import DashboardLayout from "../layout/DashboardLayout";
 const Contract = () => {
   return (
     <DashboardLayout>
-      <h2>Our Contract</h2>
-      <p>
-        Lorem Lorem ipsum dolor sit, amet consectetur adipisicing elit. Natus,
-        ipsam, ad quaerat deleniti quod nisi eum odit, aliquam debitis at
-        ratione a assumenda excepturi perferendis? Quibusdam natus eveniet iusto
-        sequi.
-      </p>
+      <div className="w-5/6 mx-auto text-center">
+        <h2 className="mb-5 text-9xl">Our Contract </h2>
+        <p className="max-w-screen-md mx-auto mt-20">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ullam natus
+          adipisci quam laboriosam temporibus dolorem aut quod tempore, at
+          veritatis voluptas provident architecto totam nostrum molestiae,
+          aliquam possimus asperiores molestias.
+        </p>
+      </div>
     </DashboardLayout>
   );
 };
--- src/pages/Terms.jsx
+++ src/pages/Terms.jsx
@@ -4,9 +4,9 @@ import DashboardLayout from "../layout/DashboardLayout";
 const Terms = () => {
   return (
     <DashboardLayout>
-      <div className="w-5/6 mx-auto">
-        <h2 className="mb-5 text-9xl">Your terms </h2>
-        <p>
+      <div className="w-5/6 mx-auto text-center">
+        <h2 className="mb-5 text-9xl">Our terms </h2>
+        <p className="max-w-screen-md mx-auto mt-20">
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ullam natus
           adipisci quam laboriosam temporibus dolorem aut quod tempore, at
           veritatis voluptas provident architecto totam nostrum molestiae,
