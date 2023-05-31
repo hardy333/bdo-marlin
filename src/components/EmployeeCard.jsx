5d5738e501e50adf3a1387bbf15f78fccd9e9bac
gela samsonidze <samsonidze14@gmail.com>
2023-05-31 16:55:09 +0400

    svg update

--- src/components/EmployeeCard.jsx
+++ src/components/EmployeeCard.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import dots from "../assets/employees/dots.svg";
 import { Menu, MenuItem } from "@szhsin/react-menu";
 import { motion } from "framer-motion";
+import UserSvg from "./svgs/UserSvg";
+import DotsSvg from "./svgs/DotsSvg";
 
 const EmployeeCard = ({
   name,
@@ -26,27 +28,7 @@ const EmployeeCard = ({
       className="employee-card"
     >
       <div className="employee-card__img-container">
-        <svg
-          className="employee-card__img"
-          id="Layer_3"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 60 60"
-        >
-          <defs></defs>
-          <circle className="cls-3" fill="#6e0ff5" cx="30" cy="30" r="30" />
-          <g className="cls-2">
-            <path
-              className="cls-1"
-              fill="#fff"
-              d="m46.43,40.71v2.29c0,3.57-2.89,6.46-6.46,6.46h-19.94c-3.57,0-6.46-2.89-6.46-6.46v-2.29c0-3.91,1.74-7.42,4.49-9.78,1.31-1.13,3.23-1.07,4.58.02,2.01,1.63,4.57,2.6,7.36,2.6s5.35-.97,7.36-2.6c1.35-1.09,3.26-1.14,4.58-.02,2.75,2.36,4.49,5.86,4.49,9.77Z"
-            />
-            <path
-              className="cls-1"
-              fill="#fff"
-              d="m39.59,19.9c0,2.71-1.13,5.16-2.94,6.9-1.72,1.67-4.07,2.69-6.66,2.69s-4.93-1.03-6.65-2.69c-1.82-1.74-2.95-4.19-2.95-6.9,0-5.29,4.31-9.6,9.6-9.6s9.6,4.3,9.6,9.6Z"
-            />
-          </g>
-        </svg>
+        <UserSvg />
       </div>
 
       <Menu
@@ -54,12 +36,18 @@ const EmployeeCard = ({
         align="center"
         className="employee-card-menu"
         menuButton={({ open }) => (
-          <img
+          // <img
+          //   onClick={() => setAactiveEmployee(name)}
+          //   className={`employee-card__dots ${open ? "open" : ""}`}
+          //   src={dots}
+          //   alt=""
+          // />
+          <button
             onClick={() => setAactiveEmployee(name)}
-            className={`employee-card__dots ${open ? "open" : ""}`}
-            src={dots}
-            alt=""
-          />
+            className={`employee-card__dots-btn ${open ? "open" : ""}`}
+          >
+            <DotsSvg />
+          </button>
         )}
         transition
       >
