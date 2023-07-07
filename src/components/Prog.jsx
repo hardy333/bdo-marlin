0b5f21e2a7aa37f050e8afd89ef289000eb9faa5
Gela Samsonidze <samsonidze7@yahoo.com>
2023-07-07 18:16:28 +0400

    deleted comments

--- src/components/Prog.jsx
+++ src/components/Prog.jsx
@@ -16,9 +16,6 @@ const Prog = ({progress, setProgress}) => {
         progress={progress}
         onLoaderFinished={() => setProgress(0)}
       />
-      {/* <button onClick={() => setProgress(progress + 10)}>Add 10%</button>
-      <button onClick={() => setProgress(progress + 20)}>Add 20%</button>
-      <button onClick={() => setProgress(100)}>Complete</button> */}
     </div>
   );
 };
