319aa0df023c1bebf81aef68013514d4a9921e52
Gela Samsonidze <samsonidze7@yahoo.com>
2023-07-28 18:11:48 +0400

    terms

--- src/pages/Terms.jsx
+++ src/pages/Terms.jsx
@@ -1,16 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+import termsData  from "./terms/termsData";
+import "../styles/terms.css";
+
+
+
 const Terms = () => {
+  const [terms, setTerms] = useState(termsData)
+  const [selectedTerm, setSelectedTerm] = useState(() => terms[0])
+
+  console.log(terms[8].a.split("\n"))
+  
   return (
     <>
-      <div className="w-5/6 mx-auto text-center">
-        <h2 className="mb-5 text-9xl">Our terms </h2>
-        <p className="max-w-screen-md mx-auto mt-20">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ullam natus
-          adipisci quam laboriosam temporibus dolorem aut quod tempore, at
-          veritatis voluptas provident architecto totam nostrum molestiae,
-          aliquam possimus asperiores molestias.
-        </p>
-      </div>
+      <section className="terms">
+        <h1>წესები და პირობები</h1>
+        <main className="terms__main">
+          <div className="terms__left">
+            <ul className="terms__list">
+              {
+                termsData.map((term, index) => {
+
+                console.log(term.q)
+                  return <li onClick={() => setSelectedTerm(term)} className={`terms__list-item ${selectedTerm.q === term.q ? "active" : ""}`} key={term.q}>
+                    <span className="terms__list-item__number">{index + 1}</span>
+                    <span className="terms__list-item__q">{term.q}</span>
+                  </li>
+                })
+              }
+            </ul>
+          </div>
+          <div className="terms__right">
+              {selectedTerm.a.split("\n").map(pText=> <p>{pText}</p>) }
+            
+          </div>
+        </main>
+      </section>
     </>
   );
 };
