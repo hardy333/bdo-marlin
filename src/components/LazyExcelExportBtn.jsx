fd60a01e76c7ae60ba7ea4ec0290f4bb62a31b46
Gela Samsonidze <samsonidze7@yahoo.com>
2023-09-06 09:18:00 +0400

    .

--- src/components/LazyExcelExportBtn.jsx
+++ src/components/LazyExcelExportBtn.jsx
@@ -1,28 +1,28 @@
 import React, { Suspense } from "react";
-import ExcelExportBtn from "./ExcelExportBtn";
-// const ExcelExportBtn = React.lazy(() => import("./ExcelExportBtn"));
+// import ExcelExportBtn from "./ExcelExportBtn";
+const ExcelExportBtn = React.lazy(() => import("./ExcelExportBtn"));
 
 const LazyExcelExportBtn = ({ data, name = "data" }) => {
   return (
-    // <Suspense
-    //   fallback={
-    //     <div
-    //       style={{
-    //         width: 20,
-    //         height: 20,
-    //         background: "red",
-    //         display: "flex",
-    //         alignItems: "center",
-    //         justifyContent: "center",
-    //         border: "5px solid green",
-    //       }}
-    //     >
-    //       abc
-    //     </div>
-    //   }
-    // >
+    <Suspense
+      fallback={
+        <div
+          style={{
+            width: 20,
+            height: 20,
+            background: "red",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            border: "5px solid green",
+          }}
+        >
+          abc
+        </div>
+      }
+    >
       <ExcelExportBtn data={data} name={name} />
-    // </Suspense>
+    </Suspense>
   );
 };
 
