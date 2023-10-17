c3e7b738cbcd2ed09aa8d9f3d7dd48ab1c1d1a82
Gela Samsonidze <samsonidze7@yahoo.com>
2023-10-17 16:58:40 +0400

    login api url

--- src/hooks/useLogin.js
+++ src/hooks/useLogin.js
@@ -13,7 +13,7 @@ export const useLogin = () => {
     setError(null);
 
     try {
-      const response = await fetch("https://10.0.0.202:5001/api/Auth/login", {
+      const response = await fetch("https://api.marlin.ge/api/Auth/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username: email, password: password }),
